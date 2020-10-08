5b111bc3e290af2dfbece74ae93aed2602341775
Jagriti <jagriti.agni@gmail.com>
2020-10-08 20:41:52 +0530

    [Jagriti]REFACTORED: generic class takes 3 generic variables and find maximum

--- src/main/java/com/bl/junitMood/FindMaxGeneric/FindMaximum.java
+++ src/main/java/com/bl/junitMood/FindMaxGeneric/FindMaximum.java
@@ -1,13 +1,21 @@
 package com.bl.junitMood.FindMaxGeneric;
 
-public class FindMaximum {
-	public boolean printWelcomeMsg() {
+public class FindMaximum <E extends Comparable<E>> {
+	E firstObj, secondObj, thirdObj;
+	
+	public FindMaximum (E first, E second, E third) {
+		this.firstObj = first;
+		this.secondObj = second;
+		this.thirdObj = third;
+	}
+	
+	public static boolean printWelcomeMsg() {
 		System.out.println("Welcome to the find maximum using generics problem.");
 		return true;
 	}
 
 	// single method to calculate maximum for Integer, Float or String
-	public <E extends Comparable<E>> E findMaximum(E firstItem, E secondItem, E thirdItem) {
+	public static <E extends Comparable<E>> E findMaximum(E firstItem, E secondItem, E thirdItem) {
 		System.out.println("Find max of: " + firstItem + " " + secondItem + " " + thirdItem);
 		E maximum = firstItem;
 		if (maximum.compareTo(secondItem) < 0)
@@ -15,5 +23,9 @@ public class FindMaximum {
 		if (maximum.compareTo(thirdItem) < 0)
 			maximum = thirdItem;
 		return maximum;
+	}
+	
+	public E findMaximum() {
+		return findMaximum(this.firstObj, this.secondObj, this.thirdObj);
 	}
 }
--- src/test/java/com/bl/junitMood/FindMaxGeneric/FindMaximumTest.java
+++ src/test/java/com/bl/junitMood/FindMaxGeneric/FindMaximumTest.java
@@ -1,84 +1,93 @@
 package com.bl.junitMood.FindMaxGeneric;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FindMaximumTest {
-
-	FindMaximum findMax = null;
-
-	@Before
-	public void initialize() {
-		findMax = new FindMaximum();
-	}
-
+	//to print welcome message
 	@Test
 	public void whenCallWelcomeMethod_shouldReturnTrue() {
-		boolean welcomeReturns = findMax.printWelcomeMsg();
+		boolean welcomeReturns = FindMaximum.printWelcomeMsg();
 		Assert.assertEquals(true, welcomeReturns);
 	}
 
 	// UC1- TC1 finds max Integer when max Integer is at first position
 	@Test
 	public void whenMaxIntegerAtFirstPlace_returnMaxNumber() {
-		Integer maxNum = findMax.findMaximum(25, 4, 16);
+		Integer maxNum = FindMaximum.findMaximum(25, 4, 16);
 		Assert.assertEquals((Integer) 25, maxNum);
 	}
 
 	// UC1- TC2 finds max Integer when max Integer is at second position
 	@Test
 	public void whenMaxIntegerAtSecondPlace_returnMaxNumber() {
-		Integer maxNum = findMax.findMaximum(25, 84, 16);
+		Integer maxNum = FindMaximum.findMaximum(25, 84, 16);
 		Assert.assertEquals((Integer) 84, maxNum);
 	}
 
 	// UC1- TC3 finds max Integer when max Integer is at third position
 	@Test
 	public void whenMaxIntegerAtThirdPlace_returnMaxNumber() {
-		Integer maxNum = findMax.findMaximum(25, 84, 116);
+		Integer maxNum = FindMaximum.findMaximum(25, 84, 116);
 		Assert.assertEquals((Integer) 116, maxNum);
 	}
 
 	// UC2- TC1 finds max float when max float is at first position
 	@Test
 	public void whenMaxFloatAtFirstPlace_returnMaxNumber() {
-		Float maxNum = findMax.findMaximum(25.3f, 12.12f, 16.8f);
+		Float maxNum = FindMaximum.findMaximum(25.3f, 12.12f, 16.8f);
 		Assert.assertEquals((Float) 25.3f, maxNum);
 	}
 
 	// UC2- TC2 finds max float when max float is at second position
 	@Test
 	public void whenMaxFloatAtSecondPlace_returnMaxNumber() {
-		Float maxNum = findMax.findMaximum(25.3f, 32.12f, 16.8f);
+		Float maxNum = FindMaximum.findMaximum(25.3f, 32.12f, 16.8f);
 		Assert.assertEquals((Float) 32.12f, maxNum);
 	}
 
 	// UC2- TC3 finds max float when max float is at third position
 	@Test
 	public void whenMaxFloatAtThirdPlace_returnMaxNumber() {
-		Float maxNum = findMax.findMaximum(25.3f, 32.12f, 86.8f);
+		Float maxNum = FindMaximum.findMaximum(25.3f, 32.12f, 86.8f);
 		Assert.assertEquals((Float) 86.8f, maxNum);
 	}
 
 	// UC3- TC1 finds max String when max String is at first position
 	@Test
 	public void whenMaxStringAtFirstPlace_returnMaxString() {
-		String maxString = findMax.findMaximum("Peach", "Banana", "Apple");
+		String maxString = FindMaximum.findMaximum("Peach", "Banana", "Apple");
 		Assert.assertEquals("Peach", maxString);
 	}
 
 	// UC3- TC2 finds max String when max String is at second position
 	@Test
 	public void whenMaxStringAtSecondPlace_returnMaxString() {
-		String maxString = findMax.findMaximum("Banana", "Peach", "Apple");
+		String maxString = FindMaximum.findMaximum("Banana", "Peach", "Apple");
 		Assert.assertEquals("Peach", maxString);
 	}
 
 	// UC3- TC3 finds max String when max String is at third position
 	@Test
 	public void whenMaxStringAtThirdPlace_returnMaxString() {
-		String maxString = findMax.findMaximum("Banana", "Apple", "Peach");
+		String maxString = FindMaximum.findMaximum("Banana", "Apple", "Peach");
+		Assert.assertEquals("Peach", maxString);
+	}
+	
+	//UC3-Refactor2- test case for using generic class
+	@Test
+	public void integersGivenInGenericClass_shouldReturnMaxInt() {
+		Integer maxInt = new FindMaximum<Integer>(25, 4, 16).findMaximum();
+		Assert.assertEquals((Integer) 25, maxInt);
+	}
+	@Test
+	public void floatGivenInGenericClass_shouldReturnMaxFloat() {
+		Float maxFloat = new FindMaximum<Float>(25.4f, 44.5f, 16.10f).findMaximum();
+		Assert.assertEquals((Float) 44.5f, maxFloat);
+	}
+	@Test
+	public void stringsGivenInGenericClass_shouldReturnMaxString() {
+		String maxString = new FindMaximum<String>("Banana", "Apple", "Peach").findMaximum();
 		Assert.assertEquals("Peach", maxString);
 	}
 }
