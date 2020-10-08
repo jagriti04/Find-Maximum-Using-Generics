383485e453dd2a36e56a1556884fd690cfe8570c
Jagriti <jagriti.agni@gmail.com>
2020-10-08 18:03:44 +0530

    [Jagriti]ADDED: method and test cases for finding the max integer out of 3

--- src/main/java/com/bl/junitMood/FindMaxGeneric/FindMaximum.java
+++ src/main/java/com/bl/junitMood/FindMaxGeneric/FindMaximum.java
@@ -5,4 +5,14 @@ public class FindMaximum {
 		System.out.println("Welcome to the find maximum using generics problem.");
 		return true;
 	}
+
+	// find the max integer out of three given integers
+	public Integer findMaxInteger(Integer firstInt, Integer secondInt, Integer thirdInt) {
+		Integer maxNum = firstInt;
+		if (maxNum.compareTo(secondInt) < 0)
+			maxNum = secondInt;
+		if (maxNum.compareTo(thirdInt) < 0)
+			maxNum = thirdInt;
+		return maxNum;
+	}
 }
--- src/test/java/com/bl/junitMood/FindMaxGeneric/FindMaximumTest.java
+++ src/test/java/com/bl/junitMood/FindMaxGeneric/FindMaximumTest.java
@@ -18,4 +18,22 @@ public class FindMaximumTest {
 		boolean welcomeReturns = findMax.printWelcomeMsg();
 		Assert.assertEquals(true, welcomeReturns);
 	}
+	//UC1- TC1 finds max int when max int is at first position
+	@Test
+	public void whenMaxIntegerAtFirstPlace_returnThatNumber() {
+		Integer maxNum = findMax.findMaxInteger(25, 4, 16);
+		Assert.assertEquals((Integer) 25, maxNum);
+	}
+	//UC1- TC2 finds max int when max int is at second position
+	@Test
+	public void whenMaxIntegerAtSecondPlace_returnThatNumber() {
+		Integer maxNum = findMax.findMaxInteger(25, 84, 16);
+		Assert.assertEquals((Integer) 84, maxNum);
+	}
+	//UC1- TC3 finds max int when max int is at third position
+	@Test
+	public void whenMaxIntegerAtThirdPlace_returnThatNumber() {
+		Integer maxNum = findMax.findMaxInteger(25, 84, 116);
+		Assert.assertEquals((Integer) 116, maxNum);
+	}
 }
